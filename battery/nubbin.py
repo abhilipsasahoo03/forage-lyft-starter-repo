bf97008cef4dac126cc28118153f25f954c6e508
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2022-06-02 14:53:34 +0530

    Create nubbin.py

---
+++ battery/nubbin.py
@@ -0,0 +1,11 @@
+from battery.battery import Battery
+from utils import add_years_to_date
+
+class Nubbin(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+    def needs_service(self):
+        schedule_service_date = add_years_to_date(self.last_service_date,4)
+        if schedule_service_date < self.current_date: return True
+        return False
